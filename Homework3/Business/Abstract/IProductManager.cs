62cdadd8a75b6145bfda5815503b5210ecccc8c9
Ege Sefertaş <egesefertas@gmail.com>
2024-02-20 00:53:41 +0300

    Add: Business Layer

---
+++ Homework3/Business/Abstract/IProductManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Homework3.Entities.Abstract;
+
+namespace Homework3.Business.Abstract
+{
+    public interface IProductManager
+    {
+        List<IEntity> GetAll();
+        void Add(IEntity entity);
+        void Update(IEntity entity);
+        void Delete(IEntity entity);
+    }
+}
---
+++ Homework3/Business/Concrete/InstructorManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Homework3.Business.Abstract;
+using Homework3.Entities.Abstract;
+using Homework3.DataAccess.Abstract;
+using Homework3.DataAccess.Concrete;
+
+namespace Homework3.Business.Concrete
+{
+    public class InstructorManager : IProductManager
+    {
+        private IProductDal _productDal;
+
+        public InstructorManager(InstructorDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Add(IEntity entity)
+        {
+            _productDal.Add(entity);
+        }
+
+        public void Delete(IEntity entity)
+        {
+            _productDal.Delete(entity);
+        }
+
+        public List<IEntity> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+
+        public void Update(IEntity entity)
+        {
+            _productDal.Update(entity);
+        }
+    }
+}
